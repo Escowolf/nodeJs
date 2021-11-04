d832773ebc1017b25044ea8cefd2cdeaf27fe1e8
Ezequiel Sousa Silva <63311919+Escowolf@users.noreply.github.com>
2021-11-03 22:15:48 -0300

    atualizar master

--- project_02/Modulo.js
+++ project_02/Modulo.js
@@ -4,11 +4,11 @@ const app = express(); //instanciando o app para utilizar node
 var calc = require("./Funcoes");
 
 app.get("/quadrado/", function(req,res){
-    res.send("<h1>A resposta é: "+calc.areaQuadrado(8,8)+"</h1>"); 
+    res.send("<h1>A área do quadrado é: "+calc.areaQuadrado(8,8)+"</h1>"); 
  })
 
 app.get("/triangulo/",function(req,res){
-    res.send("<h1>A área do triângulo é: "+calc.areaTriangulo(2,4)"</h1>");
+    res.send("<h1>A área do triângulo é: "+calc.areaTriangulo(2,4)+"</h1>");
 })
 
 app.listen(3000,function(erro){  // cria a aplicação na porta 4000 e chama a função de callback
