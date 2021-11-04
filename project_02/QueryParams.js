101d9bf1448f7ae74c7888d8a5b68c3b9faa284c
Ezequiel Sousa Silva <63311919+Escowolf@users.noreply.github.com>
2021-11-03 23:09:46 -0300

    update message

--- project_02/QueryParams.js
+++ project_02/QueryParams.js
@@ -13,13 +13,15 @@ app.get("/idade/", function(req,res){
     //req --> dados enviados pelo cliente
     var id = req.query["id"];
 
-    if (id<18){
+    if (id>0 && id<18){
         res.send("<h1>Você é de menor! Não pode acessar essa página.</h1>")
-    }else if(id>=18){
+    }else if(id>=18 && id<100){
         //res --> resposta enviada pelo servidor de volta ao cliente
         res.send("<h1>Mesmo depois dos 18 você ainda é jovem,<br> afinal são apenas "+id+" anos!</h1>");
+    }else if(id>=100){
+        res.send("<h1>É impressionante você estar viva! Vida longa à rainha!</h1>")
     }else{
-        console.log("Nenhum valor foi digitado!")
+        res.send("<h1>Você não digitou um valor válido.</h1>");
     }
    
     
